cf121c4160748b5a1681c6fc7178630d4bcc3425
John Zhou <drzhouq@gmail.com>
2016-10-29 06:13:55 -0700

    my first attempt

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,27 @@
 $(function() {
+        $.ajax({url:'https://www.codeschool.com/users/131425',
+                        dataType:'jsonp',
+                        success: function(result) {
+                                $.each(result.courses.completed, function(courseCompleted) {
+                                        var badge = $('<div></div>');
+                                        badge.addStyle('course');
+                                        var title = $('<h3></h3>');
+                                        title.text( courseCompleted.title);
+                                        var image = $('<img></img>');
+                                        image.attr('src', courseCompleted.badge);
+                                        var button =$('<a></a>');
+                                        button.text('See Course');
+                                        button.attr('href',courseCompleted.url);
+                                        button.attr('target','_blank');
+                                        button.addStyle('btn btn-Primary');
+                                        badge.append(title);
+                                        badge.append(image);
+                                        badge.append(button);
+                                        $('#badges').append(badge);
+                                });
 
+                        }
+                        });
   // your code will go here
 
 });
